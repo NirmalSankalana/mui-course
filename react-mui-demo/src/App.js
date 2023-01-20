436677c99568bd6445f5e92298cf6675cd2218af
Nirmal Sankalana <nirmal.s@eyepax.com>
2023-01-20 10:54:03 +0530

    MUI Typography

--- react-mui-demo/src/App.js
+++ react-mui-demo/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import { MuiTypography } from "../src/components/MuiTypography.tsx";
 
 function App() {
   return (
     <div className="App">
-    
+      <MuiTypography />
     </div>
   );
 }
---
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>Header 1</Typography>
+        <Typography variant='h2'>Header 2</Typography>
+        <Typography variant='h3'>Header 3</Typography>
+        <Typography variant='h4'>Header 4</Typography>
+        <Typography variant='h5'>Header 5</Typography>
+        <Typography variant='h6'>Header 6</Typography>
+    </div>
+  )
+}
