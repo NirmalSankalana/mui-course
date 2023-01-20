b29c8fa03abe9d05070530f3a1f7cad14dbdc060
NirmalSankalana <sankalana.nirmal@gmail.com>
2023-01-20 15:15:34 +0530

    MUI Button components

--- react-mui-demo/src/App.js
+++ react-mui-demo/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import { MuiTypography } from "../src/components/MuiTypography.tsx";
+// import { MuiTypography } from "../src/components/MuiTypography.tsx";
+import { MuiButton } from "../src/components/MuiButton.tsx";
+
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      <MuiButton />
     </div>
   );
 }
