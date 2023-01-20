bcfbfb2b9c372c77ce1b7582ac26c4b56c412056
Nirmal Sankalana <nirmal.s@eyepax.com>
2023-01-20 19:25:26 +0530

    MUI Button Group

--- react-mui-demo/src/App.js
+++ react-mui-demo/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 // import { MuiTypography } from "../src/components/MuiTypography.tsx";
 import { MuiButton } from "../src/components/MuiButton.tsx";
 
-
 function App() {
   return (
     <div className="App">
--- react-mui-demo/src/components/MuiButton.tsx
+++ react-mui-demo/src/components/MuiButton.tsx
@@ -1,51 +1,81 @@
 import React from "react";
-import { Stack, Button, IconButton } from "@mui/material";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import SendIcon from '@mui/icons-material/Send';
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import SendIcon from "@mui/icons-material/Send";
 
 export const MuiButton = () => {
-    return (
-        <Stack>
-            <Stack spacing={2} direction="row">
-                <Button variant="text" href="https://nirmalsankalana.github.io">
-                    Text
-                </Button>
-                <Button variant="contained">Contained</Button>
-                <Button variant="outlined">Outlined</Button>
-            </Stack>
-            <Stack spacing={2} direction="row">
-                <Button variant="contained" color="primary">
-                    Primary
-                </Button>
-                <Button variant="contained" color="secondary">
-                    Secondary
-                </Button>
-                <Button variant="contained" color="info">
-                    Info
-                </Button>
-                <Button variant="contained" color="warning">
-                    Warning
-                </Button>
-                <Button variant="contained" color="error">
-                    Error
-                </Button>
-                <Button variant="contained" color="success">
-                    Success
-                </Button>
-            </Stack>
-            <Stack display="block" spacing={2} direction="row">
-                <Button variant="contained" size="small">small</Button>
-                <Button variant="contained" size="medium">medium</Button>
-                <Button variant="contained" size="large">large</Button>
-            </Stack>
-            <Stack spacing={2} direction='row'>
-                <Button variant="contained"><AddCircleOutlineIcon /></Button>
-                <Button variant="contained" startIcon={<SendIcon />} disableElevation>send</Button>
-                <Button variant="contained" endIcon={<SendIcon />} disableRipple onClick={() => alert("Clicked")}>send</Button>
-                <IconButton color="success" size="small">
-                    <AddCircleOutlineIcon />
-                </IconButton>
-            </Stack>
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://nirmalsankalana.github.io">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          small
+        </Button>
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">
+          <AddCircleOutlineIcon />
+        </Button>
+        <Button variant="contained" startIcon={<SendIcon />} disableElevation>
+          send
+        </Button>
+        <Button
+          variant="contained"
+          endIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("Clicked")}
+        >
+          send
+        </Button>
+        <IconButton color="success" size="small">
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("Left Clicked")}>Left</Button>
+          <Button>Centered</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+    </Stack>
+  );
 };
